3092b26a510ebbe118b73a4f7c31fca30fc432ce
Theresa Campbell <t.campbell@sasktel.net>
2018-06-27 22:19:01 +0000

    Reformatted app.js with proper indentation.

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,22 +1,22 @@
 var map;
-      var toronto = {lat: 43.6532, lng: -79.3832};
+var toronto = {lat: 43.6532, lng: -79.3832};
 
-      function initMap() {
-        map = new google.maps.Map(
-          document.getElementById('map'), {
-            zoom: 4,
-            center: toronto
-        });
+function initMap() {
+  map = new google.maps.Map(
+    document.getElementById('map'), {
+      zoom: 4,
+      center: toronto
+  });
 
+  function placeMarker(location) {
+    marker = new google.maps.Marker({
+      position: location,
+      map: map,
+      title: 'The 6ix',
+      draggable: true
+    });
+  }
 
-      function placeMarker(location) {
-        marker = new google.maps.Marker({
-          position: location,
-          map: map,
-          title: 'The 6ix',
-          draggable: true
-        });
-      }
+placeMarker(toronto);
 
-      placeMarker(toronto);
-      }
+}
