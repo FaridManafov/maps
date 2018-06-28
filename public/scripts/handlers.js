3bdc9aa59b1a8c0da010ca01b8a17659beda1e9c
jonathandannel <usixjad@gmail.com>
2018-06-28 20:50:17 +0000

    added validator function for registration

---
+++ public/scripts/handlers.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+
+  $('#registration-form').on('submit', function(e) {
+    let errors = [];
+    let notAllowed = "~!@#$%^&*()_+=-;''|{}[]?><.,"
+
+    let username = $('#username').val().split('');
+    let password = $('#userpassword').val().split('');
+
+    let userNotAllowed = false;
+    let pwNotAllowed = false;
+
+    username.forEach((letter) => {
+      if (notAllowed.includes(letter)) {
+        userNotAllowed = true;
+      }
+    });
+
+    password.forEach((letter) => {
+      if (notAllowed.includes(letter)) {
+        pwNotAllowed = true;
+      }
+    });
+
+    if (userNotAllowed) {
+      errors.push('Username must not contain any special characters');
+    } else if (pwNotAllowed) {
+      errors.push('Password must not contain any special characters');
+    }
+
+    if (username.length < 5) {
+      errors.push('Username must be at least 5 characters');
+    } else if (username.length > 10) {
+      errors.push('Username must not be longer than 10 characters');
+    } else if (password.length < 5) {
+      errors.push('Password must be at least 5 characters')
+    } else if (password.length > 10) {
+      errors.push('Password must not be longer than 10 characters')
+    }
+
+    if (errors.length !== 0) {
+      console.log(errors);
+      $('#error-field').empty();
+      e.preventDefault();
+
+      errors.forEach((err) => {
+        let $errorMsg = $(`<div class="alert alert-danger" role="alert"> ${err} </div>`);
+        $('#error-field').append($errorMsg);
+      })
+
+      errors = [];
+    } else {
+      console.log(username, password);
+      username.join('');
+      password.join('');
+      console.log(username, password);
+    }
+
+  });
+});
--- server/server.js
+++ server/server.js
@@ -1,17 +1,14 @@
 const express = require('express');
-const app = express();
 const PORT = 3000;
 const path = require('path');
-const cookieSession = require('cookie-session');
 const bodyParser = require("body-parser");
-const bcrypt = require('bcrypt-nodejs');
 const nodeSassMiddleware = require('node-sass-middleware');
+const app = express();
 
 /* Database Config */
 const knexConfig = require('./knexfile.js')
 const knex = require('knex')(knexConfig.development);
 
-/* Use middleware */
 app.set("view engine", "ejs");
 
 app.use(nodeSassMiddleware({
@@ -22,10 +19,6 @@ app.use(nodeSassMiddleware({
 }));
 
 app.use(bodyParser.urlencoded({extended: true}));
-app.use(cookieSession({
-  name: 'session',
-  keys: ['']
-}));
 app.use(express.static(path.join(__dirname, '../public')));
 
 /* Routes */
@@ -40,29 +33,13 @@ app.get("/new", (req, res) => {
 
 app.get("/register", (req, res) => {
   res.render("register");
-  /* insert table username email password */
 });
 
 app.post("/register", (req, res) => {
   console.log('REQ.BODY.USERNAME:', req.body.username);
   console.log('REQ.BODY.USERPASSWORD:', req.body.userpassword);
-
-  let validLogin = req.body.
-
-
-
-
-
-
-
-
-
-
-
-
   res.redirect("/");
 });
-
 
 /* Start */
 
