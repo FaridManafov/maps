2b415e18e2948c7ecf0cbb7357fcb02754954817
jonathandannel <usixjad@gmail.com>
2018-06-28 00:49:21 +0000

    migration and create tables

--- server/migrations/20180627234956_create_movies.js
+++ server/migrations/20180627234956_create_movies.js
@@ -1,8 +1,18 @@
-
 exports.up = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.createTable('users', function(table) {
+      table.increments('id').primary();
+      table.string('username');
+      table.string('email');
+      table.string('password');
+      table.integer('maps_created');
+      table.integer('maps_contributing');
+    })
+  ])
 };
 
 exports.down = function(knex, Promise) {
-  
+  return Promise.all([
+    knex.schema.dropTable('users')
+  ])
 };
