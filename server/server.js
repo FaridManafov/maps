027d288ba009e394727e41bda1de0e6cff58f3a5
jonathandannel <usixjad@gmail.com>
2018-06-29 01:53:53 +0000

    fixed navbar with flex

--- server/server.js
+++ server/server.js
@@ -98,18 +98,7 @@ app.post('/logout', (req, res) => {
   res.redirect('/');
 })
 
-//Once we have map id's to test with, this route will change to "/maps/:id"
-app.get("/maps/id", (req, res) => {
-  res.render("display_map");
-});
-
-//Once we have user id's to test with, this route will change to "/users/:id"
-app.get("/users/id", (req, res) => {
-  res.render("profile");
-})
-
 /* Start */
-
 app.listen(PORT, () => {
   console.log(` => * Maps server listening on port ${PORT} *`);
 });
