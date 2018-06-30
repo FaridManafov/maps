29011d31fada4a0edea3826211ff69a22e81d1c5
jonathandannel <usixjad@gmail.com>
2018-06-30 02:22:59 +0000

    displaying lat and long in console

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -155,6 +155,7 @@ $("#new-map-submission").on("submit", function (event) {
           markerLat: marker.markerLat,
           markerLng: marker.markerLng
         }
+        console.log(data);
       })
     })
   })
@@ -167,7 +168,12 @@ $("#new-map-submission").on("submit", function (event) {
 //   $.ajax({
 //     method: "PUT",
 //     url: "/maps",
-//     data
+//     data: {
+//       mapId: req.params.id,
+//       markerLat: marker.markerLat,
+//       markerLng: marker.markerLng
+//     }
+//     console.log("updated: ", data);
 //   })
 // })
 
