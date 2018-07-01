d5dfb4fbed23f388830cbc395eea5b85753f632a
farid manafov <faridbardium@gmail.com>
2018-07-01 00:04:08 +0000

    Displays addresses on display map

--- public/scripts/display-map.js
+++ public/scripts/display-map.js
@@ -40,13 +40,14 @@ function geocodeLatLng(geocoder, map, infowindow, lat, lng) {
         //here is where the info is relayed in formatted address style
 
         var address = results[0].formatted_address;
-        infowindow.setContent(address);//!!
-        appendStagedMarker(address)
+        appendStagedMarker(address);
+        // infowindow.setContent(address);//disabled as this is the infowindow bubble on marker that doesnt exist
+
         console.log(address);
         //Jquery into the html
         $('#result-address').text(address)
 
-        infowindow.open(map, marker);
+        // infowindow.open(map, marker);//disabled as this is the infowindow bubble click listener on marker that doesnt exist
       } else {
         window.alert('No results found');
       }
@@ -56,4 +57,8 @@ function geocodeLatLng(geocoder, map, infowindow, lat, lng) {
   });
 }
 
+function appendStagedMarker(location) {
+  let listItem = `<li class="list-group-item list-group-item-action">${location}</li>`
+  $("#displayMapMarkers").append(listItem);
+}
 console.log("array of aids", )
