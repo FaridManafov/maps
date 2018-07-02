c42f287a77cee1f416dbfe861fecc4f443cdc384
Theresa Campbell <t.campbell@sasktel.net>
2018-07-02 19:36:26 +0000

    Deleted some comments. Added readme. Added screenshots to docs directory.

---
+++ README.md
@@ -0,0 +1,33 @@
+# MyMaps
+
+MyMaps is a full stack web application built with Express, Bootstrap, and Knex. It allows users to create, save, and favourite personalized maps with as many points as they want.
+
+## Final Product
+
+!["Screenshot of homepage."]()
+!["Screenshot of a user's map."]()
+!["Screenshot 1/2 of functions that initialize maps using the Google Maps API."]()
+!["Screenshot 2/2 of functions that initialize maps using the Google Maps API."]()
+!["Screenshot of POST route for the registration page."]()
+
+## Dependencies
+
+-  bcrypt-nodejs
+-  body-parser
+-  bootstrap
+-  cookie-session
+-  ejs
+-  express
+-  jquery
+-  knex
+-  moment
+-  node-sass-middleware
+-  nodemon
+-  pg
+-  popper.js
+-  sass
+
+## Getting Started
+
+- Install all dependencies (using the `npm install` command in the terminal).
+- Run the development web server using the `npm start` command in the terminal. The project runs on localhost:3000.
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -33,19 +33,12 @@ function initMap() {
       position: event.latLng,
       map: map,
       animation: google.maps.Animation.DROP
-
     });
 
     //FETCH lat lng from click
     var markerLat = marker.getPosition().lat();
     var markerLng = marker.getPosition().lng();
     stagedMarkers.push({markerLat, markerLng});
-    console.log("staged:", stagedMarkers)
-
-
-    //THIS HAS THE LAT LONG DATA CONSOLE LOGGED, SEND THIS TO THE COLLECTION
-    console.log(markerLat)
-    console.log(markerLng)
 
     geocodeLatLng(geocoder, map, infowindow, markerLat, markerLng);
 
@@ -62,17 +55,6 @@ function initMap() {
   });
 }
 
-// function loadMarkerPayload(){
-
-//   $.ajax({
-//     method: 'GET',
-//     url: /maps/markers,
-//     success: function(markerArray){
-
-//     }
-//   })
-// }
-
 function submitMarkerPayload(markerArray) {
   $.ajax({
     method: 'POST',
@@ -83,20 +65,10 @@ function submitMarkerPayload(markerArray) {
   })
 }
 
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-// When a user creates a maker I want to push an item to the stagedMarkers List
-
-// Then I want to re render the marker list of the right
-
-
-
 function appendStagedMarker(location) {
   let listItem = `<li class="list-group-item list-group-item-action">${location}</li>`;
   $("#newMapMarkers").append(listItem);
 }
-
-
 
 function sendMarkers(mapID) {
   var payload = {
@@ -104,11 +76,7 @@ function sendMarkers(mapID) {
     mapId: "asd"
   }
   var body = JSON.stringify(payload)
-
-  // Send over the body
-
 }
-// ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 //reverse geocoding
 function geocodeLatLng(geocoder, map, infowindow, lat, lng) {
@@ -116,15 +84,6 @@ function geocodeLatLng(geocoder, map, infowindow, lat, lng) {
   geocoder.geocode({'location': location}, function(results, status) {
     if (status === 'OK') {
       if (results[0]) {
-
-        // var marker = new google.maps.Marker({
-        //   position: location,
-        //   map: map,
-        //   label: labels[labelIndex++ % labels.length],
-        //   animation: google.maps.Animation.DROP
-        // });
-
-        //here is where the info is relayed in formatted address style
 
         var address = results[0].formatted_address;
         appendStagedMarker(address)
@@ -139,7 +98,6 @@ function geocodeLatLng(geocoder, map, infowindow, lat, lng) {
     }
   });
 }
-
 //SUBMITTING WITH 'SAVE' BUTTON ON '/NEW'
 $("#new-map-submission").on("submit", function (event) {
 
@@ -175,31 +133,6 @@ $("#new-map-submission").on("submit", function (event) {
   })
 })
 
-//SUBMITTING EDITED VERSION OF MAP WITH 'SAVE' BUTTON ON '/MAPS/:ID' (WORK IN PROGRESS)
-// $("#edited-map-submssion").on("submit", function (event) {
-//   event.preventDefault();
-
-//   $.ajax({
-//     method: "PUT",
-//     url: "/maps",
-//     data: {mapId: req.params.id}
-//   }).done(function(data) {
-//     console.log("edited data: ", data);
-
-//     stagedMarkers.forEach(function(marker) {
-//       $.ajax({
-//         method: "PUT",
-//         url: "/markers",
-//         data: {
-//           mapId: mapId,
-//           markerLat: marker.markerLat,
-//           markerLng: marker.markerLng
-//         }
-//       })
-//     })
-//   })
-// })
-
 // SUBMITTING MAP TO A USER'S FAVORITES WITH 'FAVORITE' BUTTON ON '/MAPS/:ID' (WORK IN PROGRESS)
 $("#favorite-map-submssion").on("submit", function (event) {
   event.preventDefault();
@@ -214,124 +147,3 @@ $("#favorite-map-submssion").on("submit", function (event) {
     console.log("Data from pushing 'favorite' button: ", data, "\nStatus: ", status)
   })
 });
-
-
-  //callback
-  // geocodeLatLng(geocoder, map, infowindow);
-  //TEST
-  //   document.getElementById('submit').addEventListener('click', function() {
-  //   geocodeLatLng(geocoder, map, infowindow);
-  // });
-
-
-// //saves the marker information
-// function saveData() {
-//   var name = escape(document.getElementById("name").value);
-//   var address = escape(document.getElementByIdtoronto("address").value);
-//   var type = document.getElementById("type").value;
-//   var latlng = marker.getPosition();
-//   var url = "phpsqlinfo_addrow.php?name=" + name + "&address=" + address +
-//             "&type=" + type + "&lat=" + latlng.lat() + "&lng=" + latlng.lng();
-
-//   downloadUrl(url, function(data, responseCode) {
-
-//     if (responseCode == 200 && data.length <= 1) {
-//       infowindow.close();
-//       messagewindow.open(map, marker);
-//     }
-//   });
-// }
-
-
-
-
-      // //this is what marker info we should pull from the database, just the lat: latitudeDB and lng:LongitudeDB,
-      // //the information of the location will be fetched by reverse geocoding
-
-
-      // //this initializes the map
-      // //centre is where the map will be centered
-      // function initMap() {
-      //   map = new google.maps.Map(
-      //     document.getElementById('map'), {
-      //       zoom: 12,
-      //       center: toronto,
-      //   });
-
-      //   //this places the marker on the location that is entered, you can run this multiple times for different places
-      // function placeMarker(location) {
-      //   marker = new google.maps.Marker({
-      //     position: location,
-      //     map: map,
-      //     title: 'The 6ix',
-      //     draggable: true,
-      //     animation: google.maps.Animation.DROP,
-      //     label: labels[labelIndex++ % labels.length]
-      //   });
-
-      // // on click listener pin which shows information of the pin above it
-      // marker.addListener('click', function() {
-      //   infowindow.open(map, marker);
-      // });
-
-      // //lines below is the info for when you press the pin, place this into the database for when the user inputs
-      // //information into the new pin
-
-      //   var contentString = '<div id="content">'+
-      //   '<div id="siteNotice">'+
-      //   '</div>'+
-      //   '<h1 id="firstHeading" class="firstHeading">Uluru</h1>'+
-      //   '<div id="bodyContent">'+
-      //   '<p><b>Uluru</b>, also referred to as <b>Ayers Rock</b>, is a large ' +
-      //   'sandstone rock formation in the southern part of the '+
-      //   'Northern Territory, central Australia. It lies 335&#160;km (208&#160;mi) '+
-      //   'south west of the nearest large town, Alice Springs; 450&#160;km '+
-      //   '(280&#160;mi) by road. Kata Tjuta and Uluru are the two major '+
-      //   'features of the Uluru - Kata Tjuta National Park. Uluru is '+
-      //   'sacred to the Pitjantjatjara and Yankunytjatjara, the '+
-      //   'Aboriginal people of the area. It has many springs, waterholes, '+
-      //   'rock caves and ancient paintings. Uluru is listed as a World '+
-      //   'Heritage Site.</p>'+
-      //   '<p>Attribution: Uluru, <a href="https://en.wikipedia.org/w/index.php?title=Uluru&oldid=297882194">'+
-      //   'https://en.wikipedia.org/w/index.php?title=Uluru</a> '+
-      //   '(last visited June 22, 2009).</p>'+
-      //   '</div>'+
-      //   '</div>';
-
-
-      //   //this initializes the window when the click listener is activated on the ping, which pulls up content above,
-      //   //decide if we need info boxes or not
-      //   var infowindow = new google.maps.InfoWindow({
-      //     content: contentString
-      //   });
-
-      // }
-
-      // function geocodePlaceId(geocoder, map, infowindow) {
-      //   var placeId = document.getElementById('place-id').value;
-      //   geocoder.geocode({'placeId': placeId}, function(results, status) {
-      //     if (status === 'OK') {
-      //       if (results[0]) {
-      //         map.setZoom(11);
-      //         map.setCenter(results[0].geometry.location);
-      //         var marker = new google.maps.Marker({
-      //           map: map,
-      //           position: results[0].geometry.location  <!-- <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBB8Rzd9fg-Ef8JwpUWcyz9jCdXRSqJWCM&callback=initMap"></script> -->
-
-      //         });
-      //         infowindow.setContent(results[0].formatted_address);
-      //         infowindow.open(map, marker);
-      //       } else {
-      //         window.alert('No results found');
-      //       }
-      //     } else {
-      //       window.alert('Geocoder failed due to: ' + status);
-      //     }
-      //   });
-      // }
-
-
-
-      // placeMarker(toronto);
-      // geocodePlaceId(toronto)
-      // }
